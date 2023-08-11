b4499d2b9ef198167ada97849cc0649c948584fc
Sourish Sarmah <sourishsarmah@gmail.com>
2023-08-11 18:58:25 +0530

    added database query to get store wokring hours

--- app/crud/store.py
+++ app/crud/store.py
@@ -3,11 +3,9 @@ import pandas as pd
 
 class StoreRepo:
     @classmethod
-    def get_data_from_id(cls, session, store_id, timestamp):
+    def get_data_from_id(cls, session, store_id, timestamp) -> pd.DataFrame:
         results_df = pd.read_sql_query(
             """
-            select * from 
-            (
                 select 
                 b.store_id, 
                 b.timestamp_utc, 
@@ -15,13 +13,14 @@ class StoreRepo:
                 b.timestamp_local::time as time_local,
                 status, 
                 b."day", 
-                coalesce(store_hours.start_time_local, '00:00:00') as start_time_local, 
-                coalesce(store_hours.end_time_local, '23:59:00') as end_time_local from 
+                b.timezone_str 
+                from 
                 (
                     select 
                     a.store_id, 
                     status, 
                     timestamp_utc at TIME zone 'UTC' as timestamp_utc,
+                    coalesce(store_tz.timezone_str, 'America/Chicago') as timezone_str, 
                     timestamp_utc AT TIME ZONE coalesce(store_tz.timezone_str, 'America/Chicago') as timestamp_local,
                     extract(dow from (timestamp_utc AT TIME ZONE coalesce(store_tz.timezone_str, 'America/Chicago'))) as "day"
                     from 
@@ -29,16 +28,11 @@ class StoreRepo:
                         SELECT store_status.store_id, timestamp_utc, status
                         from store_status 
                         where store_status.store_id = %(store_id)s 
-                        and timestamp_utc between %(current_timestamp)s::timestamp - interval '1 week' and %(current_timestamp)s
+                        and timestamp_utc between %(current_timestamp)s::date - interval '1 week' and %(current_timestamp)s
                     ) a
                     left join store_tz 
                     on a.store_id = store_tz.store_id
-                ) as b
-                left join store_hours 
-                on b.store_id = store_hours.store_id
-                and b."day" = store_hours."day"
-            ) as c
-            where time_local between start_time_local and end_time_local """,
+                ) as b """,
             con=session.get_bind(),
             params={"store_id": store_id, "current_timestamp": timestamp},
         )
@@ -46,9 +40,21 @@ class StoreRepo:
         return results_df
 
     @classmethod
-    def get_all_store_ids(cls, session):
+    def get_all_store_ids(cls, session) -> list:
         results = session.execute("SELECT distinct store_id FROM store_status")
 
         results = [result[0] for result in results]
 
         return results
+
+    @classmethod
+    def get_working_hours(cls, session, store_id) -> pd.DataFrame:
+        results_df = pd.read_sql_query(
+            """
+                select * from store_hours 
+                where store_id = %(store_id)s """,
+            con=session.get_bind(),
+            params={"store_id": store_id},
+        )
+
+        return results_df
