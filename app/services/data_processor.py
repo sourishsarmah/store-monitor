2b46baf383d3c9401f852e1ea765afeb377192ca
Sourish Sarmah <sourishsarmah@gmail.com>
2023-08-12 00:27:21 +0530

    added the store report generation flow

--- app/services/data_processor.py
+++ app/services/data_processor.py
@@ -1,4 +1,7 @@
+from datetime import datetime, time, timedelta
+
 import pandas as pd
+import pytz
 
 from app.crud.store import StoreRepo
 from app.db.session import SessionLocal
@@ -12,30 +15,121 @@ class StoreDataProcessor:
             store_repo = StoreRepo()
             store_ids = store_repo.get_all_store_ids(session)
 
+            curr_timstamp_utc = datetime.fromisoformat(current_timestamp).replace(
+                tzinfo=pytz.utc
+            )
+
             for store_id in store_ids:
                 print(f"Procession Store Data for store_id: {store_id}")
 
                 store_hours_data = store_repo.get_working_hours(session, store_id)
+                store_hours_data = cls.__process_working_hours(
+                    store_hours_data, store_id
+                )
                 store_status_data = store_repo.get_data_from_id(
                     session, store_id, current_timestamp
                 )
 
-                store_status_data["timezone_str"].iloc[0]
+                store_timezone_str = store_status_data["timezone_str"].iloc[0]
+
+                curr_timestamp_local = curr_timstamp_utc.astimezone(
+                    pytz.timezone(store_timezone_str)
+                )
                 processed_data = cls.__process_store_data(
                     store_status_data, store_hours_data
                 )
-                store_report = cls.extract_info(processed_data)
+                store_report = cls.extract_info(processed_data, curr_timestamp_local)
                 yield store_report
 
     @classmethod
-    def extract_info(cls, processed_store_data: pd.DataFrame) -> ReportOutput:
-        pass
+    def extract_info(
+        cls, processed_store_data: pd.DataFrame, current_timestamp
+    ) -> ReportOutput:
+
+        uptime_last_hour, downtime_last_hour = cls.__extract_last_hour_info(
+            processed_store_data, current_timestamp
+        )
+
+        report = {
+            "store_id": processed_store_data["store_id"].iloc[0],
+            "uptime_last_hour": uptime_last_hour,
+            "uptime_last_day": cls.__extract_last_day_info(
+                processed_store_data[processed_store_data["status"] == "active"],
+                current_timestamp,
+            ),
+            "update_last_week": cls.__extract_last_week_info(
+                processed_store_data[processed_store_data["status"] == "active"]
+            ),
+            "downtime_last_hour": downtime_last_hour,
+            "downtime_last_day": cls.__extract_last_day_info(
+                processed_store_data[processed_store_data["status"] == "inactive"],
+                current_timestamp,
+            ),
+            "downtime_last_week": cls.__extract_last_week_info(
+                processed_store_data[processed_store_data["status"] == "inactive"]
+            ),
+        }
+
+        return report
+
+    @classmethod
+    def __extract_last_week_info(cls, dataframe: pd.DataFrame) -> int:
+        last_week_data = dataframe["time_diff"].sum() / 60
+        return last_week_data
+
+    @classmethod
+    def __extract_last_day_info(
+        cls, dataframe: pd.DataFrame, curr_timestamp_local
+    ) -> int:
+        last_day = (curr_timestamp_local - timedelta(days=1)).date()
+        last_day_df = dataframe[dataframe["date_local"] == last_day]
+        last_day_data = last_day_df["time_diff"].sum() / 60
+        return last_day_data
+
+    @classmethod
+    def __extract_last_hour_info(
+        cls, dataframe: pd.DataFrame, curr_timestamp_local
+    ) -> (int, int):
+        last_hour_timestamp = curr_timestamp_local - timedelta(hours=1)
+        last_hour_date = last_hour_timestamp.date()
+        last_hour = last_hour_timestamp.hour
+        last_hour_df = dataframe[(dataframe["date_local"] == last_hour_date)]
+        last_hour_df = last_hour_df[
+            (
+                (last_hour_df["time_local"] >= time(hour=last_hour))
+                & (last_hour_df["time_local"] < time(hour=last_hour + 1))
+            )
+            | (
+                (last_hour_df["status_end_time"] >= time(hour=last_hour))
+                & (last_hour_df["status_end_time"] < time(hour=last_hour + 1))
+            )
+        ]
+
+        if len(last_hour_df):
+            last_hour_df["time_local"].iloc[0] = time(hour=last_hour)
+            last_hour_df["status_end_time"].iloc[-1] = time(hour=last_hour + 1)
+            last_hour_df["time_diff"] = last_hour_df[
+                ["status_end_time", "time_local"]
+            ].apply(lambda x: cls.__cal_time_diff(x), axis=1)
+            last_active_hour_data = last_hour_df[last_hour_df["status"] == "active"][
+                "time_diff"
+            ].sum()
+            last_inactive_hour_data = last_hour_df[
+                last_hour_df["status"] == "inactive"
+            ]["time_diff"].sum()
+        else:
+            last_active_hour_data = 60
+            last_inactive_hour_data = 0
+
+        return last_active_hour_data, last_inactive_hour_data
 
     @classmethod
     def __process_store_data(
         cls, store_data: pd.DataFrame, store_hours: pd.DataFrame
     ) -> pd.DataFrame:
         store_agg_work_hours = cls.__agg_store_working_hours(store_hours)
+
+        store_id = store_data["store_id"].iloc[0]
 
         store_df = store_data.merge(
             store_hours,
@@ -90,21 +184,44 @@ class StoreDataProcessor:
             status_df["status_end_time"] = status_df["time_local"].shift(-1)
             status_df = status_df[status_df["status"] != "closing"]
 
-            def cal_time_diff(row):
-                """
-                Calculate the active/inactive interval duration in minutes
-                """
-                dummy_date = datetime(2023, 1, 1)
-                diff  = datetime.combine(dummy_date, row["status_end_time"]) - datetime.combine(dummy_date, row["time_local"])
-                return diff.total_seconds()/60
-            
-            status_df["time_diff"] = status_df[["status_end_time", "time_local"]].apply(lambda x: cal_time_diff(x), axis=1)
+            status_df["time_diff"] = status_df[["status_end_time", "time_local"]].apply(
+                lambda x: cls.__cal_time_diff(x), axis=1
+            )
 
             status_df["date_local"] = idx[0]
             status_df["day"] = idx[1]
+
+            status_df["store_id"] = store_id
+
+            def get_timestamp_local(row):
+                return datetime.combine(row[0], row[1])
+
+            status_df["start_timestamp_local"] = status_df[
+                ["date_local", "time_local"]
+            ].apply(lambda x: get_timestamp_local(x), axis=1)
+            status_df["end_timestamp_local"] = status_df[
+                ["date_local", "status_end_time"]
+            ].apply(lambda x: get_timestamp_local(x), axis=1)
             processed_dfs.append(status_df)
 
         return pd.concat(processed_dfs)
+
+    @classmethod
+    def __process_working_hours(cls, data: pd.DataFrame, store_id) -> pd.DataFrame:
+        days_of_week = list(range(0, 7))
+        days = data["day"].values.tolist()
+        remaining_days = list(set(days_of_week) - set(days))
+
+        default_data = pd.DataFrame(
+            {
+                "store_id": [store_id] * len(remaining_days),
+                "day": remaining_days,
+                "start_time_local": [time(0, 0)] * len(remaining_days),
+                "end_time_local": [time(23, 59, 59)] * len(remaining_days),
+            }
+        )
+        store_hours_df = pd.concat([data, default_data], ignore_index=True)
+        return store_hours_df
 
     @classmethod
     def __agg_store_working_hours(
@@ -113,6 +230,7 @@ class StoreDataProcessor:
         """
         combines multiple working shifts of the store into an aggregated dataframe
         """
+        print(store_working_hours)
         store_agg_hours_df = (
             store_working_hours.groupby(["store_id", "day"]).agg(list).reset_index()
         )
@@ -123,3 +241,14 @@ class StoreDataProcessor:
         store_agg_hours_df = store_agg_hours_df[["store_id", "day", "working_shifts"]]
 
         return store_agg_hours_df
+
+    @staticmethod
+    def __cal_time_diff(row):
+        """
+        Calculate the active/inactive interval duration in minutes
+        """
+        dummy_date = datetime(2023, 1, 1)
+        diff = datetime.combine(dummy_date, row["status_end_time"]) - datetime.combine(
+            dummy_date, row["time_local"]
+        )
+        return diff.total_seconds() / 60
