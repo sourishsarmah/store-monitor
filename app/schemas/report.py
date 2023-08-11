70dfa8b65836caa338ba64014761d98d79876081
Sourish Sarmah <sourishsarmah@gmail.com>
2023-08-11 21:56:03 +0530

    fixed report schema

--- app/schemas/report.py
+++ app/schemas/report.py
@@ -16,7 +16,7 @@ class ReportOutput:
     store_id: int
     uptime_last_hour: int
     uptime_last_day: int
-    update_last_week: int
+    uptime_last_week: int
     downtime_last_hour: int
     downtime_last_day: int
     downtime_last_week: int
