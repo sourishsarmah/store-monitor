c7c2fbaf3f1e2fd0a61ed1c32fb4ec3b52bd0893
Sourish Sarmah <sourishsarmah@gmail.com>
2023-08-12 20:30:53 +0530

    changed report status source from redis queue to postgres database table

--- app/api/api_v1/endpoints/report.py
+++ app/api/api_v1/endpoints/report.py
@@ -1,32 +1,30 @@
 from typing import Any
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app import schemas
-from app.core.celery_app import celery_app
+from app.api.deps import get_db
+from app.crud.report import ReportRepo
 from app.worker import generate_report
 
 router = APIRouter()
 
 
 @router.get("/get_report/{report_id}", response_model=schemas.ReportResponse)
-def read_users(report_id: str) -> Any:
+def read_users(report_id: str, db=Depends(get_db)) -> Any:
     """
     Retrieve users.
     """
-    task_result = celery_app.AsyncResult(report_id)
-    result = {
-        "report_id": report_id,
-        "report_status": task_result.status,
-        "report_result": task_result.result,
-    }
-    return result
+    report = ReportRepo.get(db, report_id)
+    print(report.__dict__)
+    return report.__dict__
 
 
 @router.post("/trigger_report", response_model=schemas.Report)
-def create_user() -> Any:
+def trigger_report(db=Depends(get_db)) -> Any:
     """
     Create new user.
     """
     task = generate_report.delay()
+    ReportRepo().create(db, task.id)
     return {"report_id": task.id}
